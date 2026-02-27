cf253fb4698b2a7ba749aa8b7a937df172911d19
Maximilian Winter <maximilian.winter.91@gmail.com>
2026-02-27 19:34:26 +0100

    Further Clean up

--- src/pytonium_python_framework/generate_pyi.py
+++
@@ -1,3 +0,0 @@
-# The .pyi stub file (Pytonium/pytonium.pyi) is now hand-maintained.
-# This script is no longer used for generation.
-# Edit Pytonium/pytonium.pyi directly when adding or changing public API methods.
